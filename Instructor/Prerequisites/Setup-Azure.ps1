892490e06e79d56f7e48679959d56b279779db44
Roman Korecky <Roman.Korecky@easyon.at>
2025-02-07 00:07:54 +0100

    Enhances Setup-Azure script to support customizable CSV delimiter and encoding; improves user creation and authentication reset logic

--- Instructor/Prerequisites/Setup-Azure.ps1
+++ Instructor/Prerequisites/Setup-Azure.ps1
@@ -1,8 +1,29 @@
 [CmdletBinding()]
 param (
     [string]
-    $CsvPath = (Join-Path -Path $PSScriptRoot -ChildPath 'users.csv')
+    $CsvPath,
+    [char]
+    $Delimiter = ';',
+    [ValidateSet(
+        'ascii',
+        'ansi',
+        'bigendianunicode',
+        'bigendianutf32',
+        'oem',
+        'unicode',
+        'utf7',
+        'utf8',
+        'utf8BOM',
+        'utf8NoBom',
+        'utf32'
+    )]
+    [string]
+    $Encoding = 'default'
 )
+
+if ([string]::IsNullOrEmpty($CsvPath)) {
+    $CsvPath = (Join-Path -Path $PSScriptRoot -ChildPath 'users.csv')
+}
     
 
 #region Install required modules
@@ -18,7 +39,7 @@ if (-not (Get-Module -ListAvailable -Name Az.Accounts)) {
 #endregion Install required modules
 
 # Read user names from a CSV
-$users = Import-Csv -Path $CsvPath
+$users = Import-Csv -Path $CsvPath -Delimiter $Delimiter -Encoding $Encoding
 
 # Prepare an array to store the results
 $results = @()
@@ -45,96 +66,141 @@ foreach ($user in $users) {
     $userPrincipalName = "$($user.UserName)@$domain"
     $mgUser = Get-MgUser -Filter "userPrincipalName eq '$userPrincipalName'"
 
-    # If user not found, create a new user
-    if ($null -eq $mgUser) {
-        $mgUser = New-MgUser `
-            -DisplayName $user.DisplayName `
-            -UserPrincipalName $userPrincipalName `
-            -AccountEnabled $true
-    }
-
-    # If user found, update the display name
+    # If user found, update the display name and reset authentication
     if ($null -ne $mgUser) {
+        Write-Host "Updating display name for $userPrincipalName"
         Update-MgUser -UserId $mgUser.Id -DisplayName $user.DisplayName
-    }
 
-    #region Reset the password
-    $authenticationMethodId = (
-        Get-MgUserAuthenticationPasswordMethod -UserId $mgUser.Id).Id
-    $newPassword = (
-        Reset-MgUserAuthenticationMethodPassword `
-            -UserId $mgUser.Id `
-            -AuthenticationMethodId $authenticationMethodId
-    ).Password
-    #endregion Reset the password
+        #region Reset the password
+        Write-Host "Resetting password for $userPrincipalName"
+        $authenticationMethodId = (
+            Get-MgUserAuthenticationPasswordMethod -UserId $mgUser.Id).Id
+        $newPassword = (
+            Reset-MgUserAuthenticationMethodPassword `
+                -UserId $mgUser.Id `
+                -AuthenticationMethodId $authenticationMethodId
+        ).NewPassword
+        #endregion Reset the password
 
-    #region Remove authentication methods other than password to reset MFA
+        #region Remove authentication methods other than password to reset MFA
 
-    $authenticationMethodId = Get-MgUserAuthenticationEmailMethod `
-        -UserId $mgUser.Id
-    if ($authenticationMethodId) {
-        Remove-MgUserAuthenticationEmailMethod `
-            -UserId $mgUser.Id -EmailAuthenticationMethodId $authenticationMethodId
-    }
+        Write-Host "Resetting MFA for $userPrincipalName"
+        $authenticationMethodId = Get-MgUserAuthenticationEmailMethod `
+            -UserId $mgUser.Id
+        if ($authenticationMethodId) {
+            Remove-MgUserAuthenticationEmailMethod `
+                -UserId $mgUser.Id `
+                -EmailAuthenticationMethodId $authenticationMethodId
+        }
 
-    $authenticationMethodId = Get-MgUserAuthenticationFido2Method `
-        -UserId $mgUser.Id
-    if ($authenticationMethodId) {
-        Remove-MgUserAuthenticationFido2Method `
-            -UserId $mgUser.Id -Fido2AuthenticationMethodId $authenticationMethodId
-    }
+        $authenticationMethodId = Get-MgUserAuthenticationFido2Method `
+            -UserId $mgUser.Id
+        if ($authenticationMethodId) {
+            Remove-MgUserAuthenticationFido2Method `
+                -UserId $mgUser.Id `
+                -Fido2AuthenticationMethodId $authenticationMethodId
+        }
 
-    $authenticationMethodId = Get-MgUserAuthenticationPhoneMethod `
-        -UserId $mgUser.Id
-    if ($authenticationMethodId) {
+        $authenticationMethodId = Get-MgUserAuthenticationPhoneMethod `
+            -UserId $mgUser.Id
+        if ($authenticationMethodId) {
             Remove-MgUserAuthenticationPhoneMethod `
                 -UserId $mgUser.Id `
                 -PhoneAuthenticationMethodId $authenticationMethodId
-    }
+        }
 
-    $authenticationMethodId = `
-        Get-MgUserAuthenticationMicrosoftAuthenticatorMethod -UserId $mgUser.Id
-    if ($authenticationMethodId) {
-        Remove-MgUserAuthenticationMicrosoftAuthenticatorMethod `
-            -UserId $mgUser.Id `
-            -MicrosoftAuthenticatorAuthenticationMethodId `
-                $authenticationMethodId
-    }
-
-    $authenticationMethodId = Get-MgUserAuthenticationPhoneMethod `
-        -UserId $mgUser.Id
-    if ($authenticationMethodId) {
-        Remove-MgUserAuthenticationPhoneMethod `
-            -UserId $mgUser.Id `
-            PhoneAuthenticationMethodId $authenticationMethodId
-    }
-
-    $authenticationMethodId = `
-        Get-MgUserAuthenticationWindowsHelloForBusinessMethod -UserId $mgUser.Id
-    if ($authenticationMethodId) {
-            Remove-MgUserAuthenticationWindowsHelloForBusinessMethod `
+        $authenticationMethodId = `
+            Get-MgUserAuthenticationMicrosoftAuthenticatorMethod `
+                -UserId $mgUser.Id
+        if ($authenticationMethodId) {
+            Remove-MgUserAuthenticationMicrosoftAuthenticatorMethod `
                 -UserId $mgUser.Id `
-                -WindowsHelloForBusinessAuthenticationMethodId `
-                $authenticationMethodId  
+                -MicrosoftAuthenticatorAuthenticationMethodId `
+                    $authenticationMethodId
+        }
+
+        $authenticationMethodId = Get-MgUserAuthenticationPhoneMethod `
+            -UserId $mgUser.Id
+        if ($authenticationMethodId) {
+            Remove-MgUserAuthenticationPhoneMethod `
+                -UserId $mgUser.Id `
+                PhoneAuthenticationMethodId $authenticationMethodId
+        }
+
+        $authenticationMethodId = `
+            Get-MgUserAuthenticationWindowsHelloForBusinessMethod `
+                -UserId $mgUser.Id
+        if ($authenticationMethodId) {
+                Remove-MgUserAuthenticationWindowsHelloForBusinessMethod `
+                    -UserId $mgUser.Id `
+                    -WindowsHelloForBusinessAuthenticationMethodId `
+                    $authenticationMethodId  
+        }
+
+        $authenticationMethodId = Get-MgUserAuthenticationSoftwareOathMethod `
+            -UserId $mgUser.Id
+        if ($authenticationMethodId) {
+            Remove-MgUserAuthenticationSoftwareOathMethod `
+                -UserId $mgUser.Id `
+                -SoftwareOathAuthenticationMethodId $authenticationMethodId
+        }
+
+        $authenticationMethodId = `
+            Get-MgUserAuthenticationTemporaryAccessPassMethod -UserId $mgUser.Id
+        if ($authenticationMethodId) {
+            Remove-MgUserAuthenticationTemporaryAccessPassMethod `
+                -UserId $mgUser.Id `
+                -TemporaryAccessPassAuthenticationMethodId `
+                    $authenticationMethodId
+        }
+
+        #endregion Remove authentication methods other than password to reset MFA
+
     }
 
-    $authenticationMethodId = Get-MgUserAuthenticationSoftwareOathMethod `
-        -UserId $mgUser.Id
-    if ($authenticationMethodId) {
-        Remove-MgUserAuthenticationSoftwareOathMethod `
-            -UserId $mgUser.Id `
-            -SoftwareOathAuthenticationMethodId $authenticationMethodId
-    }
+    # If user not found, create a new user
+    if ($null -eq $mgUser) {
+        Write-Host "Creating user $userPrincipalName"
+        
+        <#
+            Generate a random password consisting of 1 uppercase letter, 
+            2 lowercase letters, and 5 digits
+        #>
+        $newPassword = `
+            (
+                -join (
+                    (65..91) |
+                    Get-Random -Count 1 |
+                    ForEach-Object { [char] $PSItem }
+                )
+            ) + `
+            (
+                -join (
+                    (97..122) |
+                    Get-Random -Count 2 |
+                    ForEach-Object { [char] $PSItem }
+                ) 
+            )+
+            (
+                -join (
+                    (48..57) |
+                    Get-Random -Count 5 |
+                    ForEach-Object { [char] $PSItem }
+                )
+        )
+        $passwordProfile = @{
+            Password = $newPassword
+            ForceChangePasswordNextSignIn = $false
+        }
 
-    $authenticationMethodId = Get-MgUserAuthenticationTemporaryAccessPassMethod `
-        -UserId $mgUser.Id
-    if ($authenticationMethodId) {
-        Remove-MgUserAuthenticationTemporaryAccessPassMethod `
-            -UserId $mgUser.Id `
-            -TemporaryAccessPassAuthenticationMethodId $authenticationMethodId
+        # Create the user
+        $mgUser = New-MgUser `
+            -DisplayName $user.DisplayName `
+            -UserPrincipalName $userPrincipalName `
+            -MailNickname $user.UserName `
+            -PasswordProfile $passwordProfile `
+            -AccountEnabled
     }
-
-    #endregion Remove authentication methods other than password to reset MFA
 
     # Add the result to the array
     $user.NewPassword = $newPassword
@@ -154,9 +220,11 @@ $results = @()
 
 
 #region Get a list of tenants
+
 Connect-AzAccount
 $azTenants = Get-AzTenant
 Disconnect-AzAccount
+
 #endregion Get a list of tenants
 
 # Invite the users to their tenants and add them the Global Administrator role
@@ -165,30 +233,44 @@ foreach ($user in $users) {
     $azTenant = $azTenants | Where-Object { $user.Domain -in $PSItem.Domains }
 
     # Connect to Microsoft Graph
+    Write-Host "Connecting to Microsoft Graph in tenant $($user.Domain)"
     Connect-MgGraph `
         -TenantId $azTenant.Id `
         -Scopes 'User.Invite.All', 'RoleManagement.ReadWrite.Directory'
 
     # Invite the user
+    Write-Host `
+        "Inviting user $($user.UserPrincipalName) to tenant $($user.Domain)"
     $invitation = New-MgInvitation `
         -InvitedUserEmailAddress $user.UserPrincipalName `
         -InviteRedirectUrl 'https://myapplications.microsoft.com' `
         -SendInvitationMessage:$false
 
-    # Assign the Global Administrator role to the invited user
+    #region Assign the Global Administrator role to the invited user
     $roleDefinition = Get-MgDirectoryRole | 
-    Where-Object { $_.DisplayName -eq "Global Administrator" }
+        Where-Object { $_.DisplayName -eq "Global Administrator" }
 
     if ($roleDefinition) {
-        New-MgDirectoryRoleMemberByRef `
+        $directoryRoleMember = Get-MgDirectoryRoleMember `
             -DirectoryRoleId $roleDefinition.Id `
-            -BodyParameter @{
-                '@odata.id' = `
-                    "https://graph.microsoft.com/v1.0/directoryObjects/$(
-                        $invitation.InvitedUser.Id
-                    )"
-            }
+            -Filter "Id eq '$($invitation.InvitedUser.Id)'"
+
+        if ($null -eq $directoryRoleMember) {
+            Write-Host `
+                "Assigning the Global Administrator role to $(
+                    $user.UserPrincipalName
+                )"
+            New-MgDirectoryRoleMemberByRef `
+                -DirectoryRoleId $roleDefinition.Id `
+                -BodyParameter @{
+                    '@odata.id' = `
+                        "https://graph.microsoft.com/v1.0/directoryObjects/$(
+                            $invitation.InvitedUser.Id
+                        )"
+                }
+        }
     }
+    #endregion Assign the Global Administrator role to the invited user
 
     # Add the invitation redeem URL to the results
     $user.InvitationRedeemUrl = $invitation.InvitationRedeemUrl
@@ -199,4 +281,6 @@ foreach ($user in $users) {
 }
 
 # Write back the results to the CSV file
-$results | Export-Csv -Path $csvPath -NoTypeInformation
+$results | Export-Csv `
+    -Path $csvPath -NoTypeInformation -Delimiter $Delimiter -Encoding $Encoding
+Write-Host "Results written to $csvPath"
